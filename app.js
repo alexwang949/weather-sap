f59a48e86578a5ca0768a6c29a72c0c5e8158f98
Alex Wang <alexwang949@gmail.com>
2015-08-11 11:53:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//declare app module and its dependencies
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//views: home page + forecast page
+//need: 2 pgs, routing, 2 controllers
+
+//Routes HERE
+weatherApp.config(function($routeProvider) {
+	
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'mainController'
+	})
+	.when ('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+
+});
+
+
+
+//Controllers HERE
+weatherApp.controller('mainController', ['$scope', function($scope) {
+	$scope.greeting = 'hello';
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+
+}]);
+
