761e1efe225cded4dc3366bb65405218248f1fdb
Alex Wang <alexwang949@gmail.com>
2015-08-12 12:28:49 -0400

    added route for dynamic number of days in url, added routeParams to access number of days in controller

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ weatherApp.config(function($routeProvider) {
 		templateUrl: 'pages/forecast.html',
 		controller: 'forecastController'
 	})
+	.when ('/forecast/:days', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
 
 });
 
@@ -51,7 +55,7 @@ weatherApp.controller('mainController', ['$scope', 'cityService', function($scop
 
 }]);		
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', function($scope, cityService, $resource) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function($scope, cityService, $resource, $routeParams) {
 
 	$scope.city = cityService.city;
 
@@ -61,7 +65,7 @@ weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource
 	$scope.weatherServiceApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", 
 		{ callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
-	$scope.weatherData = $scope.weatherServiceApi.get({ q: $scope.city, cnt: 2});
+	$scope.weatherData = $scope.weatherServiceApi.get({ q: $scope.city, cnt: $scope.days});
 
 	//this function converts the Kelvin temperature from the API into
 	//fahrenheit
@@ -73,6 +77,10 @@ weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource
 	$scope.convertDate = function(date) {
 		return new Date(date * 1000);
 	}
+
+	//I put the || 2 in order to provide a default value because I allowed a route that
+	//didn't pass :days
+	$scope.days = $routeParams.days || 2;
 
 
 }]);
